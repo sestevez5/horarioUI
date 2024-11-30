38559796407cf7e7bb7e6fae54b046691bd8193a
sestevez5 <sestevez5@gmail.com>
2024-11-30 18:07:31 +0000

    .

--- src/libreria/horarioUI.ts
+++ src/libreria/horarioUI.ts
@@ -1,5 +1,5 @@
 import Snap from 'snapsvg';
-import * as d3 from 'd3';
+
 
 export class HorarioUI {
 
@@ -11,11 +11,8 @@ export class HorarioUI {
 
       var s = Snap("#xaa");
       var padre = s.parent();
-      console.log(padre.node.clientHeight);
-      console.log(padre.node.clientWidth);
       s.attr({'width':padre.node.clientHeight/2, 'height': padre.node.clientWidth/2, 'fill':'red'})
-      console.log(s.node.clientHeight)
-
+      
       console.log(padre.node.style.backgroundColor);
       console.log(padre.node.attributeStyleMap);
       console.log(padre.node.computedStyleMap().getAll('background-color').toString());
