0221bb2182de87e1944bbb1cd8dd210d7b19c991
sestevez5 <sestevez5@gmail.com>
2025-01-20 19:36:39 +0000

    .

--- src/libreria/seccionesRenderizado/actividades.ts
+++ src/libreria/seccionesRenderizado/actividades.ts
@@ -1,11 +1,27 @@
 
+import { IActividad } from '../modelo';
 import { IConfiguracionGrafico } from '../parametros'
+import * as d3 from 'd3';
 
 
 
 
-export function renderizarActividades(_configuracion: IConfiguracionGrafico, _svg: any) {
+export function renderizarActividades(_configuracion: IConfiguracionGrafico, _svg: any, actividades: IActividad[]) {
 
 
+    d3.selectAll('g.panelSesionActividades').remove();
+
+    d3.selectAll('g.panelDiaSemana').nodes().forEach(
+       (nodo: any) => {
+         const actividadesACrear = actividades.filter(actG => actG.sesion.diaSemana === nodo['id']);
+         this.renderizarActividadesPorDiaSemana('g#' + nodo['id'], actividadesACrear)
+       }
+     );
+
+
+}
+
+function renderizarActividadesPorDiaSemana(panelDiaSemana: string, actividadesG: IActividad[]) {
+  console.log(panelDiaSemana);
 
 }
